9cd9adf1b195df4009010de519c1f9eca23d8487
fenek644 <fenek644@ya.ru>
2019-01-29 16:56:23 +0300

    Add page /visit and write data in the table clients
    создали стр /visit и вводимые данные записываем сначала объект класса Клиент, а затем сохраняем его в базе.

--- app.rb
+++ app.rb
@@ -15,7 +15,43 @@ class Barber < ActiveRecord::Base
 
 end
 
+before do
+
+  @barbers = Barber.all
+
+end
+
 get '/' do
-  @barbers = Barber.order "created_at desc"
+
 	erb :index
 end
+
+get '/visit' do
+  # @db = get_db
+  erb :visit
+end
+
+post '/visit' do
+
+  @user_name = params[:username]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @master = params[:master]
+  @color = params[:colorpicker]
+
+  cl = Client.new
+  cl.name = @user_name
+  cl.phone = @phone
+  cl.datestamp = @date_time
+  cl.barber = @master
+  cl.color = @color
+  cl.save
+
+
+
+  # erb :visit_mess
+  erb "OK #{@user_name}; вы записаны на #{@date_time}; ваш мастер #{@master}; выбранный цвет #{@color}"
+end
+
+
